e033a6ac2ff3ea86a094b830b03e61289a7598a3
PatrickKvartsaniy <kvartsaniy@gmail.com>
2023-01-25 17:09:50 +0100

    alembic init

---
+++ migrations/versions/c3f038820a4f_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: c3f038820a4f
+Revises: 
+Create Date: 2023-01-25 17:08:47.037082
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c3f038820a4f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
