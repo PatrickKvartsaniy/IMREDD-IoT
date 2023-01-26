c41abc854dd5f3d185e1231095c107af47c51baa
PatrickKvartsaniy <kvartsaniy@gmail.com>
2023-01-26 13:00:40 +0100

    feat: security endpoint

--- iot/router.py
+++ iot/router.py
@@ -7,7 +7,7 @@ from db import get_async_session
 iot_router = APIRouter(prefix="/iot", tags=["iot"])
 
 
-@iot_router.post("")
+@iot_router.post("/stream")
 async def data_stream(request: Request):
     req = await request.json()
     #
@@ -22,6 +22,21 @@ async def data_stream(request: Request):
     #     return Response(status_code=200)
     print(req)
 
+
+@iot_router.post("/security")
+async def security(request: Request):
+    req = await request.json()
+    #
+    # try:
+    #     schema = schemas.TelegramRequestBody(**req)
+    #     if not schema.message.text:
+    #         return Response(status_code=200)
+    #
+    #     user = await upsert_telegram_user(schema, token, db)
+    #
+    # except Exception as e:
+    #     return Response(status_code=200)
+    print(req)
 
 # async def upsert_telegram_user(schema: schemas.TelegramRequestBody, token: str, db: AsyncSession) -> models.User:
 #     telegram_member = await bot.BaseBotInterface(token).get_info(schema.message.chat.id, schema.message.from_field.id)
