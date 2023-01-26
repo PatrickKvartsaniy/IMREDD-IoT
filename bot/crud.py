0ade97ff580e790bae2a69a66cc04c94b4e274f5
PatrickKvartsaniy <kvartsaniy@gmail.com>
2023-01-26 17:49:23 +0100

    fix: telegram id field

--- bot/crud.py
+++ bot/crud.py
@@ -16,12 +16,12 @@ async def create(request: schemas.User, db: AsyncSession) -> models.User:
 
 
 async def read(platform_id: str, db: AsyncSession) -> models.User:
-    user = await db.execute(select(models.User).where(models.User.platform_id == platform_id))
+    user = await db.execute(select(models.User).where(models.User.telegram_id == platform_id))
     return user.scalar()
 
 
 async def update_subscription(platform_id: str, subscription: bool, db: AsyncSession) -> bool:
-    user = await db.execute(select(models.User).where(models.User.platform_id == platform_id))
+    user = await db.execute(select(models.User).where(models.User.telegram_id == platform_id))
     user: models.User = user.scalar()
     user.subscribed = subscription
 
@@ -35,5 +35,5 @@ async def update_subscription(platform_id: str, subscription: bool, db: AsyncSes
 
 
 async def get_all_subscribed(db: AsyncSession) -> list[models.User]:
-    users = await db.execute(select(models.User).where(models.User.subscribed == True))
+    users = await db.execute(select(models.User).where(models.User.subscribed))
     return users.scalars().all()
